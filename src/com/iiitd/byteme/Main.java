3cacb780c2d44805037dc4b24cd7c85cc06f022b
Anouska Sahoo <anouska.sahoo007@gmail.com>
2024-11-23 15:14:21 +0530

    adding package

--- src/com/iiitd/byteme/Main.java
+++ src/com/iiitd/byteme/Main.java
@@ -1,3 +1,5 @@
+package com.iiitd.byteme;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
